cae0b65f3fac91b97b1fad00006e366bfac53fad
wb-lcw229616 <wb-lcw229616@ant-financial.com>
2018-04-16 15:40:11 +0800

    说明

---
+++ src/main/java/com.sangmu/Thread/SyncThread.java
@@ -0,0 +1,46 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package com.sangmu.Thread;
+
+/**
+ * @author wb-lcw229616
+ * @version $Id: SyncThread.java, v 0.1 2018年04月12日 14:25 wb-lcw229616 Exp $
+ */
+/**
+ * 同步线程
+ */
+class SyncThread implements Runnable {
+    private static int count;
+
+    public SyncThread() {
+        count = 0;
+    }
+
+    @Override
+    public  void run() {
+        synchronized(this) {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    System.out.println(Thread.currentThread().getName() + ":" + (count++));
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public static void main(String[] args) {
+        SyncThread syncThread = new SyncThread();
+        Thread thread1 = new Thread(syncThread, "SyncThread1");
+        Thread thread2 = new Thread(syncThread, "SyncThread2");
+        thread1.start();
+        thread2.start();
+    }
+}
